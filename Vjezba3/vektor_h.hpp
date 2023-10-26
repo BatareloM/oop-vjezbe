98859dc3007cd8e419a9bd81f900faee031dd2d8
BatareloM <147716285+BatareloM@users.noreply.github.com>
2023-10-26 14:37:05 +0200

    Add files via upload

---
+++ Vjezba3/vektor_h.hpp
@@ -0,0 +1,10 @@
+#ifndef vektor_h
+#define vektor_h
+#include <vector>
+
+void unos(std::vector<int>& brojevi, int n);
+void unos(std::vector<int>& brojevi, int a, int b);
+void ispis(std::vector<int>& brojevi);
+
+
+#endif
