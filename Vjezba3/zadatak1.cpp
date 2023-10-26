ef6bbe6f50800be94b1536cee596a733ae0edc5f
BatareloM <147716285+BatareloM@users.noreply.github.com>
2023-10-26 14:39:47 +0200

    Add files via upload

---
+++ Vjezba3/zadatak1.cpp
@@ -0,0 +1,14 @@
+// Vjezba3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "vektor_h.hpp"
+
+int main()
+{
+	std::vector <int> vek1;
+	unos(vek1, 5);
+	unos(vek1, 1, 5);
+	ispis(vek1);
+
+}
