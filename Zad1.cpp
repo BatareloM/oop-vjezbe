d5a3b46bc5bfe1bfa610da0520c980f023dbde91
BatareloM <147716285+BatareloM@users.noreply.github.com>
2024-01-11 17:11:27 +0100

    Add files via upload

---
+++ Zad1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template<typename T>
+
+T mmin(T x, T y)
+{
+	return x < y ? x : y; //skripta
+}
+
+int main()
+{
+	int rezultat = mmin(5, 2);
+
+	cout << "Minimum je:" << rezultat;
+	return 0;
+}
